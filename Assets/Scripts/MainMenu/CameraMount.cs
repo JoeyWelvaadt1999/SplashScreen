ce90db141bad317a9a560ccad8f67bde404249af
SuperSaiyan4 <joeywelvaadt@gmail.com>
2015-09-01 12:41:37 +0200

    12:41

---
+++ Assets/Scripts/MainMenu/CameraMount.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMount : MonoBehaviour {
+	public Transform currentMount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, currentMount.position, 0.05f);
+		transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, 0.05f);
+	}
+
+	public void SetMount(Transform newMount) {
+		currentMount = newMount;
+	}
+
+	public void SwitchScene(int scene) {
+		Application.LoadLevel(scene);
+	}
+}
---
+++ Assets/Scripts/MainMenu/PlayMovie.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayMovie : MonoBehaviour {
+	MovieTexture mtex;
+	bool playing = true;
+	// Use this for initialization
+	void Start () {
+		
+		mtex = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		#if (!UNITY_EDITOR)
+			mtex.Play();
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(mtex.isPlaying)
+		{
+
+		} else {
+
+				Application.LoadLevel(1);
+
+		}
+	}
+
+	void OnMouseDown() {
+
+			Application.LoadLevel(1);
+
+	}
+}
